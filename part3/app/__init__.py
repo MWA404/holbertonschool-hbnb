8c132985976c3dbc821c62413a6863c692a18724
abdularham2341 <abdulrhman3334@gmail.com>
2026-08-02 11:04:43 +0300

    Implement SQLAlchemy repository

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
 from config import DevelopmentConfig
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
+db = SQLAlchemy()
 
 
 def create_app(config_class=DevelopmentConfig):
@@ -14,12 +16,18 @@ def create_app(config_class=DevelopmentConfig):
 
     bcrypt.init_app(app)
     jwt.init_app(app)
+    db.init_app(app)
 
     from app.api.v1.auth import auth_bp
     from app.api.v1.users import api as users_ns
     from flask_restx import Api
 
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+    api = Api(
+        app,
+        version='1.0',
+        title='HBnB API',
+        description='HBnB Application API'
+    )
     api.add_namespace(users_ns, path='/api/v1/users')
     app.register_blueprint(auth_bp, url_prefix='/api/v1/auth')
 
