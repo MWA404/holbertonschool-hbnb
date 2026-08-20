32b1e5961e285d706bf1f7356951b8487003e34b
MWA404 <muhannadalraddadi@gmail.com>
2026-08-20 20:18:19 +0300

    Add flask-cors to allow frontend client requests

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from config import DevelopmentConfig
 
 bcrypt = Bcrypt()
@@ -13,15 +14,13 @@ def create_app(config_class=DevelopmentConfig):
     """Application factory for Flask app configuration."""
     app = Flask(__name__)
     app.config.from_object(config_class)
-
     bcrypt.init_app(app)
     jwt.init_app(app)
     db.init_app(app)
-
+    CORS(app)
     from app.api.v1.auth import auth_bp
     from app.api.v1.users import api as users_ns
     from flask_restx import Api
-
     api = Api(
         app,
         version='1.0',
@@ -30,5 +29,4 @@ def create_app(config_class=DevelopmentConfig):
     )
     api.add_namespace(users_ns, path='/api/v1/users')
     app.register_blueprint(auth_bp, url_prefix='/api/v1/auth')
-
     return app
