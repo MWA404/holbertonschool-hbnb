857368ee0b794435d6b9591b2455a1324fd205a2
MWA404 <muhannadalraddadi@gmail.com>
2026-08-25 04:14:06 +0300

    Add amenities endpoint to part3 (ported from part2)

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -22,6 +22,7 @@ def create_app(config_class=DevelopmentConfig):
     from app.api.v1.users import api as users_ns
     from app.api.v1.places import api as places_ns
     from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.amenities import api as amenities_ns
     from flask_restx import Api
     api = Api(
         app,
@@ -32,5 +33,6 @@ def create_app(config_class=DevelopmentConfig):
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
     app.register_blueprint(auth_bp, url_prefix='/api/v1/auth')
     return app
